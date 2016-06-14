81f3fc38d5b03e887ce5d5c43eeda0efda5a7ac2
jeffersonaguilar@outlook.com <jeffersonaguilar@outlook.com>
2016-06-14 13:32:48 -0500

    fasasf

--- index.js
+++ index.js
@@ -108,7 +108,10 @@ app.post('/webhook', function (req, res) {
         if (event.message && event.message.text) {
           if (!kittenMessage(event.sender.id, event.message.text)) {
             sendMessage(event.sender.id, {text: "Mensaje de respuesta del bot para: " + event.message.text});
+          }else if (event.postback) {
+            console.log("Postback received: " + JSON.stringify(event.postback));
           }
+
         }
     }
     res.sendStatus(200);
