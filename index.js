06751a0a34fbc1c82663a36126326653047a00ac
jeffersonaguilar@outlook.com <jeffersonaguilar@outlook.com>
2016-06-14 10:22:29 -0500

    Prueba mensajes eco

--- index.js
+++ index.js
@@ -6,16 +6,20 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-var bodyParser = require('body-parser');
+
 var express = require('express');
+var bodyParser = require('body-parser');
+var request = require('request');
 // var fbsub = require("fbsub");
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 app.listen(app.get('port'));
 console.log('Se ha subido la aplicacion en el puerto 5000');
 console.log('http://localhost:5000/');
-app.use(bodyParser.json());
+//app.use(bodyParser.json());
 
 
 
@@ -49,10 +53,10 @@ app.use(bodyParser.json());
 // });
 
 
-
+// Server frontpage
 app.get('/', function(req, res) {
   console.log(req);
-  res.send('Funciona.');
+  res.send('Webhook de prueba.');
 });
 
 // app.get(['/facebook', '/instagram'], function(req, res) {
@@ -83,19 +87,49 @@ app.get('/', function(req, res) {
 
 
 
-// Prueba falaChat
+// Facebook Webhook
 app.get(['/webhook'], function(req, res) {
   if (req.query['hub.verify_token'] === 'falachattoken') {
     res.send(req.query['hub.challenge']);
     console.log('-----------------------------------------:');
     console.log(req.body);
   } else {
-    res.send('Error, wrong validation token!!!');    
+    res.send('Error, en validación de token!!!');    
     console.log('------------ERROR-----------------------------:');
   }
 });
 
 
+// handler receiving messages
+app.post('/webhook', function (req, res) {
+    var events = req.body.entry[0].messaging;
+    for (i = 0; i < events.length; i++) {
+        var event = events[i];
+        if (event.message && event.message.text) {
+            sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
+             console.log('------------Entró mensaje-----------------------------:');
+        }
+    }
+    res.sendStatus(200);
+});
 
+// generic function sending messages
+function sendMessage(recipientId, message) {
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
+        method: 'POST',
+        json: {
+            recipient: {id: recipientId},
+            message: message,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending message: ', error);
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error);
+        }
+    });
+};
 
 app.listen();
