7ecb63b59fa00284b20f4d5c3132ee51d01d4a85
jeffersonaguilar@outlook.com <jeffersonaguilar@outlook.com>
2016-06-16 10:52:33 -0500

    asdasd

--- index.js
+++ index.js
@@ -108,7 +108,7 @@ app.post('/webhook', function (req, res) {
             var request = appapi.textRequest(event.message.text);
             request.on('response', function(response) {
                 //console.log('respuesta bot:' + response);
-                sendMessage(event.sender.id, {text: 'respuesta bot:' + response['result']['resolvedQuery']['fulfillment']['speech']});
+                sendMessage(event.sender.id, {text: 'respuesta bot:' + response['result']['fulfillment']['speech']});
             });
 
             request.on('error', function(error) {
