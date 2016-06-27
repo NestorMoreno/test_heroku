5434eb4fbc84fe7cebd7052f2251155a5329848a
jeffersonaguilar@outlook.com <jeffersonaguilar@outlook.com>
2016-06-27 14:39:29 -0500

    asdasd

--- app.js
+++ app.js
@@ -96,7 +96,7 @@ function getUserInfo(id, text, time) {
             console.log('Error en send user info: ', response.body.error);
         }
         else{
-            console.log('Nombre: ' + first_name + ' ' + last_name + ' img:' + profile_pic);
+            console.log('Nombre: ' + response.first_name + ' ' + response.last_name + ' img:' + response.profile_pic);
             // DataBase
             insertData(id, text, time);
         }
